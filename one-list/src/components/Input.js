0436ff8e0d577ed29a46bd3b7141f97e53dd6ba1
Liz Tiller <lizthrilla@users.noreply.github.com>
2019-03-19 19:10:23 -0400

    start

---
+++ one-list/src/components/Input.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Input extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Form Input</h1>
+        <form>
+            <input type='text' placeholder='We will make this pretty later' />
+        </form>
+      </div>
+    )
+  }
+}
+
+export default Input;
