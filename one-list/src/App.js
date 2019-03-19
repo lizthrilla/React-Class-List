cf325fe0b10eff548275ee63406a0319faf3ebcf
Bree Jackson <bjacksonsisterwolf@gmail.com>
2019-03-19 19:32:22 -0400

    Liz just enjoys hurting Bree

--- one-list/src/App.js
+++ one-list/src/App.js
@@ -1,16 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Input from './components/Input'
+import React, { Component } from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import Input from "./components/Input"
+
+// Liz64EncodedReactClass
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      inputsubmitted: "something"
+    }
+  }
+
+  handleChange = (event) => {
+    this.setState({ inputsubmitted: event.target.value })
+  }
   render() {
     return (
       <div className="App">
-        <Input />
+        <Input inputsubmitted={this.state.inputsubmitted} handleChange={this.handleChange} />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
